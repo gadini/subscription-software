5b38c99b2a262f60037590910919d5cc93c80983
gustavo.gadini <gustavo.gadini@redeflex.com.br>
2026-02-19 11:49:35 -0300

    initial

---
+++ src/main/java/com/github/gadini/subscription_software/SubscriptionSoftwareApplication.java
@@ -0,0 +1,13 @@
+package com.github.gadini.subscription_software;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubscriptionSoftwareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubscriptionSoftwareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/gadini/subscription_software/SubscriptionSoftwareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.gadini.subscription_software;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubscriptionSoftwareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
