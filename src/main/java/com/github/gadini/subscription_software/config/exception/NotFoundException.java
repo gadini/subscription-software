ced3d549a52e59c6ac1b58def44e9e22a3d691c6
gustavo.gadini <gustavo.gadini@redeflex.com.br>
2026-02-20 11:10:35 -0300

    Adicionando NotFoundException no projeto

---
+++ src/main/java/com/github/gadini/subscription_software/config/exception/NotFoundException.java
@@ -0,0 +1,13 @@
+package com.github.gadini.subscription_software.config.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class NotFoundException extends RuntimeException {
+
+    public NotFoundException(String resource, Object identifier) {
+        super("%s with identifier %s not found".formatted(resource, identifier));
+    }
+
+}
