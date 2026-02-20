6b74a2ac7ed499cee6b2cfc04fce4945f4896a90
gustavo.gadini <gustavo.gadini@redeflex.com.br>
2026-02-20 11:09:02 -0300

    Ajuste de atributos entidade customer

--- src/main/java/com/github/gadini/subscription_software/customer/entity/Customer.java
+++ src/main/java/com/github/gadini/subscription_software/customer/entity/Customer.java
@@ -1,26 +1,36 @@
 package com.github.gadini.subscription_software.customer.entity;
 
+import com.github.gadini.subscription_software.customer.enums.CustomerStatusEnum;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDateTime;
 import java.util.Objects;
 
 @Entity(name = "customer")
-public class CustomerEntity {
+public class Customer {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @CreationTimestamp
     @Column(name = "created_at", nullable = false, updatable = false)
     private LocalDateTime createdAt;
 
+    @UpdateTimestamp
     @Column(name = "updated_at", nullable = false)
     private LocalDateTime updatedAt;
+
+    @Column(name = "name", length = 150, nullable = false)
+    private String name;
 
     @Column(name = "email", length = 150, nullable = false)
     private String email;
@@ -31,15 +41,17 @@ public class CustomerEntity {
     @Column(name = "status_id", nullable = false)
     private int statusId;
 
+    @Enumerated(EnumType.STRING)
     @Column(name = "status_name", length = 20,nullable = false)
-    private String statusName;
+    private CustomerStatusEnum statusName;
 
-    public CustomerEntity() { }
+    public Customer() { }
 
-    public CustomerEntity(Long id, LocalDateTime createdAt, LocalDateTime updatedAt, String email, String document, int statusId, String statusName) {
+    public Customer(Long id, LocalDateTime createdAt, LocalDateTime updatedAt, String name, String email, String document, int statusId, CustomerStatusEnum statusName) {
         this.id = id;
         this.createdAt = createdAt;
         this.updatedAt = updatedAt;
+        this.name = name;
         this.email = email;
         this.document = document;
         this.statusId = statusId;
@@ -70,6 +82,14 @@ public class CustomerEntity {
         this.updatedAt = updatedAt;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public String getEmail() {
         return email;
     }
@@ -94,23 +114,23 @@ public class CustomerEntity {
         this.statusId = statusId;
     }
 
-    public String getStatusName() {
+    public CustomerStatusEnum getStatusName() {
         return statusName;
     }
 
-    public void setStatusName(String statusName) {
+    public void setStatusName(CustomerStatusEnum statusName) {
         this.statusName = statusName;
     }
 
     @Override
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
-        CustomerEntity that = (CustomerEntity) o;
-        return statusId == that.statusId && Objects.equals(id, that.id) && Objects.equals(createdAt, that.createdAt) && Objects.equals(updatedAt, that.updatedAt) && Objects.equals(email, that.email) && Objects.equals(document, that.document) && Objects.equals(statusName, that.statusName);
+        Customer customer = (Customer) o;
+        return statusId == customer.statusId && Objects.equals(id, customer.id) && Objects.equals(createdAt, customer.createdAt) && Objects.equals(updatedAt, customer.updatedAt) && Objects.equals(name, customer.name) && Objects.equals(email, customer.email) && Objects.equals(document, customer.document) && Objects.equals(statusName, customer.statusName);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, createdAt, updatedAt, email, document, statusId, statusName);
+        return Objects.hash(id, createdAt, updatedAt, name, email, document, statusId, statusName);
     }
 }
